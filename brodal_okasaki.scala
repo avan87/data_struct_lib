5e1e070ec6edd4f03ca9a10a45103ea4ff1d3b5a
Val <avantrad@yandex.ru>
2016-06-16 00:12:27 +0300

    add an interface

--- brodal_okasaki.scala
+++ brodal_okasaki.scala
@@ -1,9 +1,14 @@
 import scala.collection.mutable.PriorityQueue
 
-case class BPQ(x:Int, pq:PriorityQueue[BPQ]) extends Ordered[BPQ]{
+trait Heap 
+
+case class BPQ(x:Int, pq:PriorityQueue[BPQ]) extends Ordered[BPQ] with  Heap{
 
 	def compare(that:BPQ) = that.x compare this.x
 }
+
+case object Empty extends  Heap
+
 
 
 def makeSingletonBPQ(x:Int):BPQ =  BPQ(x, new PriorityQueue())
@@ -11,7 +16,7 @@ def makeSingletonBPQ(x:Int):BPQ =  BPQ(x, new PriorityQueue())
 def merge(bpq1:BPQ, bpq2:BPQ):BPQ = {
 	if (bpq1.x < bpq2.x) BPQ(bpq1.x, bpq1.pq ++ Seq(bpq2))
 	else  BPQ(bpq2.x, bpq2.pq ++ Seq(bpq1))
-}
+}	
 
 def insert(bpq:BPQ, x:Int):BPQ = merge(bpq, makeSingletonBPQ(x))
 
@@ -23,6 +28,17 @@ def extractMin(bpq:BPQ):Tuple2[Int, BPQ] = {
 	val tuple  = Tuple2(bpq.pq.head, bpq.pq.tail)
 	Tuple2(bpq.x, BPQ(bpq.x, tuple._1.pq ++ tuple._2))
 }
+
+def deleteMin(bpq:BPQData):BPQData = bpq match {
+
+	case (bpq.pq == Empty) => Empty 
+	case _ = Empty
+}
+ 
+
+
+
+
 
 val bpq  = (1 to 100).toSeq.map(x => makeSingletonBPQ(x)).reduce((x, y) => merge(x,y))
 
